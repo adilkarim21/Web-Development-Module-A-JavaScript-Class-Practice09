dbd0c29e93d45c97324f20c419091570afb74de0
Adil Khosa <muhammadadilmemon79@gmail.com>
2024-10-15 00:48:14 +0500

    HTML & JavaScript

---
+++ app.js
@@ -0,0 +1,138 @@
+// EVENTS:
+
+
+// function addLink(anchorElement) {
+
+//     console.log(anchorElement);
+
+//     anchorElement.href = "https://github.com/adilkarim21"
+
+//     anchorElement.target = "_blank";
+
+//     anchorElement.style.color = "black";
+
+//     anchorElement.style.textDecoration = "none";
+
+// }
+
+
+
+// function removeLink(anchorElement1) {
+
+//     anchorElement1.style.color = "blue";
+
+//     anchorElement1.style.textDecoration = "underline";
+
+// }
+
+
+
+// console.log(document.getElementById('heading'));
+
+// console.log(document.getElementById('para1'));
+
+// console.log(document.getElementById('para2'));
+
+
+
+// var imageElement = document.getElementById("img");
+
+// imageElement.src = "https://pics.craiyon.com/2023-11-30/OQexP4wzQo6O071XjfzTPA.webp";
+
+// imageElement.width = "200";
+
+// imageElement.style.borderRadius = "150px";
+
+
+
+// function submit() {
+
+//     var emailElement = document.getElementById("email");
+
+//     var passwordElement = document.getElementById("password");
+
+//     if ( emailElement.value.trim() && passwordElement.value.trim() ) {
+
+//         console.log(emailElement.value);
+        
+//         console.log(passwordElement.value);
+
+//         emailElement.value = "";
+
+//         passwordElement.value = "";
+
+//     }
+
+//     else {
+
+//         alert("required fields empty..");        
+
+//     }
+
+// }
+
+
+
+
+// var paras = document.getElementsByClassName("para");
+
+// for ( var i = 0 ; i < paras.length ; i++ ) {
+
+//     paras[i].style.color = "green"; 
+
+// }
+
+
+
+
+// function gendersRadio() {
+
+//     var radios = document.getElementsByClassName("genders");
+
+//     for ( var i = 0 ; i < radios.length ; i++ ) {
+
+//         if ( radios[i].checked ) {
+
+//             console.log(radios[i].value);
+
+//         }
+
+//     }
+
+    // var maleInput = document.getElementById("Male");
+    // var femaleInput = document.getElementById("Female");
+    // var othersInput = document.getElementById("Others");
+
+    // if ( maleInput.checked ) {
+    //     console.log(maleInput.value);
+    // }
+    // else if ( femaleInput.checked ) {
+    //     console.log(femaleInput.value);
+    // }
+    // else if ( othersInput.checked ) {
+    //     console.log(othersInput.value);
+    // }
+
+// }
+
+
+
+
+// function submit() {
+
+//     var dropdown = document.getElementById("dropdown");
+
+//     console.log(dropdown.value);
+
+// }
+
+
+
+
+// function set() {
+
+//     var input = document.getElementById("imp");
+
+//     input.value = "faraz";
+
+// }
